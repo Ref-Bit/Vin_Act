13393591616fa16366f50e188db25788ea6a66d7
Ref-Bit <rsv.gho@gmail.com>
2020-07-15 17:45:19 +0300

    Initialized Products Context

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,21 @@
-// products context
+import React, { useState, createContext } from "react";
+
+export const ProductContext = createContext();
+
+export default function ProductProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [featured, setFeatured] = useState([]);
+
+  return (
+    <ProductContext.Provider
+      value={{
+        products,
+        loading,
+        featured,
+      }}
+    >
+      {children}
+    </ProductContext.Provider>
+  );
+}
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { ProductContext } from "../context/products";
 
 export default function Products() {
+  const res = React.useContext(ProductContext);
+  console.log(res);
   return <h1>hello from products page</h1>;
 }

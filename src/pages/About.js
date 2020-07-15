ef04ade9ce300a3bc02261de28d7de66e234a9c6
Ref-Bit <rsv.gho@gmail.com>
2020-07-15 17:11:05 +0300

    Initialized About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut neque mi,
+        aliquet at massa vitae, facilisis sodales arcu. In posuere, magna sit
+        amet aliquam finibus, diam orci faucibus ipsum, a vestibulum justo
+        tortor sed sapien. Maecenas nec nulla ut augue aliquam bibendum et sed
+        sapien. Proin suscipit leo lectus, quis tristique nisi malesuada eu.
+        Aliquam volutpat odio lacus, sit amet mattis odio aliquam eget. Aliquam
+        facilisis quam euismod, lacinia ex nec, lobortis ipsum. Nam ultricies
+        non lectus et porttitor. Morbi vel elit quis nisi feugiat venenatis. In
+        a tortor interdum, elementum odio eget, porttitor felis. In sit amet
+        ornare sapien, id mattis nulla. Etiam in quam nisi. Nullam euismod,
+        neque ut hendrerit sagittis, odio ante vulputate arcu, eu fringilla nibh
+        metus sed arcu. Nunc malesuada turpis et lorem viverra fermentum.
+        Aliquam nec vestibulum turpis, ac porttitor dui. Proin in ex
+        consectetur, tempor massa eu, tempor sem. Curabitur scelerisque rhoncus
+        velit at pulvinar.
+      </p>
+      <br />
+      <p>
+        Proin eget pretium diam. Maecenas at eros ullamcorper, dapibus arcu a,
+        euismod erat. Duis vehicula justo egestas dignissim feugiat. Vestibulum
+        convallis, odio at lobortis finibus, lectus mi pharetra diam, nec congue
+        odio mi nec erat. Cras imperdiet porttitor quam, in sodales ante
+        suscipit convallis. Mauris pulvinar metus non aliquam tincidunt. Nullam
+        accumsan, est eget volutpat tristique, justo enim maximus nisi, eget
+        egestas elit lacus et mauris. Donec aliquet, odio eget cursus tincidunt,
+        dui nulla tincidunt nisl, ac tincidunt mauris justo non libero. Nam
+        pellentesque risus ut quam laoreet tempor.
+      </p>
+    </section>
+  );
 }

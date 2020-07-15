11540314e91391948d50a30ef4138403adbbdd3c
Ref-Bit <rsv.gho@gmail.com>
2020-07-15 19:44:09 +0300

    Initialized ProductDetails

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,35 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { ProductContext } from "../context/products";
+import { Loading } from "../components";
 
 export default function ProductDetails() {
   let { id } = useParams();
-  return <h1>hello from product {id} details page</h1>;
+  const history = useHistory();
+  const { products } = useContext(ProductContext);
+  const product = products.find((item) => item.id === parseInt(id));
+
+  return products.length === 0 ? (
+    <Loading />
+  ) : (
+    <section className="single-product">
+      <img
+        src={product.image.url}
+        alt={product.title}
+        className="single-product-image"
+      />
+      <h1>{product.title}</h1>
+      <h2>${product.price}</h2>
+      <p>{product.description}</p>
+      <button
+        className="btn btn-primary btn-block"
+        onClick={() => {
+          // add to cart
+          history.push("/cart");
+        }}
+      >
+        add to cart
+      </button>
+    </section>
+  );
 }

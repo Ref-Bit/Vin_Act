52ee5f08ff2c4f7078229328b124cb9975c9581b
Ref-Bit <rsv.gho@gmail.com>
2020-07-17 18:47:11 +0300

    Added Restrict Access functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import CartLink from "../components/Cart/CartLink";
+import { UserContext } from "../context/user";
+import LoginLink from "../components/LoginLink";
 
 export default function Header() {
+  const { user } = useContext(UserContext);
+
   return (
     <header className="header">
       <img src={logo} alt="logo" className="logo" />
@@ -12,14 +16,21 @@ export default function Header() {
           <div>
             <li>
               <Link to="/">Home</Link>
+            </li>
+            <li>
               <Link to="/about">About</Link>
+            </li>
+            <li>
               <Link to="/products">Products</Link>
             </li>
+            {user.token && (
+              <li>
+                <Link to="/checkout">Checkout</Link>
+              </li>
+            )}
           </div>
           <div>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
+            <LoginLink />
             <CartLink />
           </div>
         </ul>

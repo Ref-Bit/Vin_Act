42d060f0c6f6ea6ffe9cc517f99ce9d191c49b0a
Ref-Bit <rsv.gho@gmail.com>
2020-07-17 17:30:52 +0300

    Added Register/Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import loginUser from "../strapi/loginUser";
+import registerUser from "../strapi/registerUser";
 
 export default function Login() {
-  return <h1>hello from login page</h1>;
+  const history = useHistory();
+  // Setup user context
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("default");
+  const [isMember, setIsMember] = useState(true);
+
+  let isEmpty = !email || !password || !username;
+
+  const toggleMember = () => {
+    setIsMember((prevMember) => {
+      let isMember = !prevMember;
+      isMember ? setUsername("default") : setUsername("");
+      return isMember;
+    });
+  };
+  const handleSubmit = async (e) => {
+    // alert
+    e.preventDefault();
+    let res;
+    if (isMember) {
+      // res = await loginUser
+    } else {
+      // res = await registerUser
+    }
+    if (res) {
+      //
+    } else {
+      //
+    }
+  };
+
+  return (
+    <section className="form section">
+      <h2 className="section-title">{isMember ? "Sign In" : "Sign Up"}</h2>
+      <form className="login-form">
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {!isMember && (
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        )}
+        {isEmpty && (
+          <p className="form-empty">please fill all the empty fields.</p>
+        )}
+        {!isEmpty && (
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        )}
+        <p className="register-link">
+          {isMember ? "need to register" : "already a member"}
+          <button type="button" onClick={toggleMember}>
+            Click Here
+          </button>
+        </p>
+      </form>
+    </section>
+  );
 }
